f804bde6bf1a56f6c8a32ddd633864375c34acc6
peterimade <peterimade6@gmail.com>
2025-01-02 16:56:56 +0100

    add home, categories, and pagennotfound components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { CategoryComponent } from './components/category/category.component';
+import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'home', component: HomeComponent},
+    { path: 'categories', component: CategoryComponent },
+    { path: '', component: HomeComponent, pathMatch: 'full' },
+    {path: '**', component: PagenotfoundComponent}
+];
