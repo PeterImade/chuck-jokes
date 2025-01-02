cdda9f637847306ead5b75560e4fe0a603aadbf1
peterimade <peterimade6@gmail.com>
2025-01-02 17:28:47 +0100

    add return statements to the service methods

--- src/app/services/jokes.service.ts
+++ src/app/services/jokes.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Joke, SearchedJoke } from '../models/Joke';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,19 +11,19 @@ export class JokesService {
   baseUrl = "https://api.chucknorris.io/jokes"
   constructor(private httpClient: HttpClient) { }
 
-  fetchRandomJoke() {
-    this.httpClient.get<Joke>(`${this.baseUrl}/random`)
+  fetchRandomJoke(): Observable<Joke> {
+    return this.httpClient.get<Joke>(`${this.baseUrl}/random`);
   }
 
-  fetchCategories() {
-    this.httpClient.get<string[]>(`${this.baseUrl}/categories`)
+  fetchCategories(): Observable<string[]> {
+    return this.httpClient.get<string[]>(`${this.baseUrl}/categories`)
   }
 
-  fetchRandomJokeFromCategory(category: string) {
-    this.httpClient.get<Joke>(`${this.baseUrl}/${category}`)
+  fetchRandomJokeFromCategory(category: string): Observable<Joke> {
+    return this.httpClient.get<Joke>(`${this.baseUrl}/${category}`)
   }
 
-  fetchSearchedJoke(searchInput: string) {
-    this.httpClient.get<SearchedJoke>(`${this.baseUrl}/search?query=${searchInput}`)
+  fetchSearchedJoke(searchInput: string): Observable<SearchedJoke> {
+    return this.httpClient.get<SearchedJoke>(`${this.baseUrl}/search?query=${searchInput}`)
   }
 }
